512541fc3b7b8bd50a3b0edce0ecc09177399739
Brice Favre <brice.favre@gmail.com>
2016-02-09 13:42:19 +0100

    Create actions, reducers and store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
 import TodoApp from './containers/TodoApp'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+
+let store = createStore(todoApp)
 
 var data = [
   {id: 1, text: "Do The App", completed: 0},
